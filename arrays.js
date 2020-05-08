230ecd4c5d43204c156c3c670c3d9b8201645937
Leamsi Escribano <leamsii.e@gmail.com>
2020-05-08 02:32:42 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,9 +1,9 @@
 let chocolateBars = ["snickets", "hundred grand", "kitkat", "skittles"]
 
 function addElementToBeginningOfArray(arr, element) {
-  return arr.unshift(element)
+  return [element, ...arr]
 }
 
 function destructivelyAddElementToBeginningOfArray(arr, element) {
-  
+  return arr.unshift(element)
 }
